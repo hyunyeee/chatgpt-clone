9d336e6abdcd6bc507ee08d2b678bcc17bef7398
hyunyeee <5004ohy@naver.com>
2025-05-15 03:10:40 +0900

    feat: Open AI API 연동 - Vercel AI SDK

--- components/chat/Chat.tsx
+++ components/chat/Chat.tsx
@@ -1,39 +1,34 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
+import { useChat } from "@ai-sdk/react";
+import { useModelStore } from "@/store/model";
 import { ArrowUp } from "lucide-react";
 import { Empty } from "./Empty";
 import { Message } from "./Message";
 import { AutoResizingTextarea } from "@/components/chat/AutoResizingTextarea";
 import { Button } from "@/components/ui/button";
-import { DUMMY_LONG_TEXT } from "@/constants/dummy";
-
-const MESSAGE_DUMMY = [
-  { id: "1", content: "더미데이터1", role: "user" },
-  { id: "2", content: "더미데이터2", role: "assistant" },
-  { id: "3", content: "더미데이터3", role: "user" },
-  { id: "4", content: DUMMY_LONG_TEXT, role: "assistant" },
-];
 
 export function Chat() {
-  const [value, setValue] = useState("");
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const model = useModelStore((state) => state.model);
   const scrollRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (scrollRef.current) {
       scrollRef.current.scrollIntoView({ behavior: "smooth" });
     }
-  }, []);
+  }, [messages]);
 
   return (
     <div className="flex flex-col w-[80%] h-full mx-auto">
       {/* 채팅영역 */}
       <div className="flex-1">
-        {MESSAGE_DUMMY.length === 0 ? (
+        {messages.length === 0 ? (
           <Empty />
         ) : (
           <>
-            {MESSAGE_DUMMY.map((message) => (
+            {messages.map((message) => (
               <Message
                 key={message.id}
                 name={"user"}
@@ -47,11 +42,11 @@ export function Chat() {
 
       {/* input영역 */}
       <div className="pb-5 sticky bottom-0 bg-white">
-        <form className="flex items-center justify-center gap-4">
-          <AutoResizingTextarea
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-          />
+        <form
+          className="flex items-center justify-center gap-4"
+          onSubmit={(e) => handleSubmit(e, { data: { model } })}
+        >
+          <AutoResizingTextarea value={input} onChange={handleInputChange} />
           <Button type="submit" size="icon">
             <ArrowUp />
           </Button>
