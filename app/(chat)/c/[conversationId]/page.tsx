6ac76c60effa0aaeaba2c3d3e1289ac181f541fb
hyunyeee <5004ohy@naver.com>
2025-05-15 07:07:19 +0900

    fix: Props 타입 수정

--- app/(chat)/c/[conversationId]/page.tsx
+++ app/(chat)/c/[conversationId]/page.tsx
@@ -2,14 +2,12 @@ import { Chat } from "@/components/chat/Chat";
 import { getMessagesByConversation } from "@/data/conversation";
 
 type Props = {
-  params: {
-    conversationId: string;
-  };
+  params: Promise<{ conversationId: string }>;
 };
 
-export default async function ConversationPage({
-  params: { conversationId },
-}: Props) {
+export default async function ConversationPage({ params }: Props) {
+  const { conversationId } = await params;
+
   const messages = await getMessagesByConversation(conversationId);
   return <Chat initialMessages={messages} />;
 }
