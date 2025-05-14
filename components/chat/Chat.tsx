bfe162b1c2ff81ff7fad169eabe5a89260f41759
hyunyeee <5004ohy@naver.com>
2025-05-14 17:54:12 +0900

    feat: Input 마크업

--- components/chat/Chat.tsx
+++ components/chat/Chat.tsx
@@ -1,5 +1,11 @@
+"use client";
+
 import { Empty } from "./Empty";
 import { Message } from "./Message";
+import { useState } from "react";
+import { AutoResizingTextarea } from "@/components/chat/AutoResizingTextarea";
+import { Button } from "@/components/ui/button";
+import { ArrowUp } from "lucide-react";
 
 const MESSAGE_DUMMY = [
   { id: "1", content: "더미데이터1", role: "user" },
@@ -7,6 +13,8 @@ const MESSAGE_DUMMY = [
 ];
 
 export function Chat() {
+  const [value, setValue] = useState("");
+
   return (
     <div className="flex flex-col w-[80%] h-full mx-auto">
       {/* 채팅영역 */}
@@ -28,6 +36,17 @@ export function Chat() {
       </div>
 
       {/* input영역 */}
+      <div className="pb-5">
+        <form className="flex items-center justify-center gap-4">
+          <AutoResizingTextarea
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <Button type="submit" size="icon">
+            <ArrowUp />
+          </Button>
+        </form>
+      </div>
     </div>
   );
 }
