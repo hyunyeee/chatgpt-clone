0aa04c48b332c24b3466fa511363c3353c70bb6c
hyunyeee <5004ohy@naver.com>
2025-05-14 17:52:37 +0900

    feat: 채팅 영역 마크업

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -1,3 +1,5 @@
+import { Chat } from "@/components/chat/Chat";
+
 export default function ChatPate() {
-  return <>Chat Page</>;
+  return <Chat />;
 }
---
+++ components/chat/Chat.tsx
@@ -0,0 +1,33 @@
+import { Empty } from "./Empty";
+import { Message } from "./Message";
+
+const MESSAGE_DUMMY = [
+  { id: "1", content: "더미데이터1", role: "user" },
+  { id: "2", content: "더미데이터2", role: "assistant" },
+];
+
+export function Chat() {
+  return (
+    <div className="flex flex-col w-[80%] h-full mx-auto">
+      {/* 채팅영역 */}
+      <div className="flex-1">
+        {MESSAGE_DUMMY.length === 0 ? (
+          <Empty />
+        ) : (
+          <>
+            {MESSAGE_DUMMY.map((message) => (
+              <Message
+                key={message.id}
+                name={"user"}
+                content={message.content}
+                role={message.role}
+              />
+            ))}
+          </>
+        )}
+      </div>
+
+      {/* input영역 */}
+    </div>
+  );
+}
